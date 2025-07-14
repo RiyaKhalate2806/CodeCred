1779c263e6567bbe21af3a39738f2d467eba1082
RiyaKhalate2806 <riyakhalate289@gmail.com>
2025-07-14 22:29:00 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 28;
+        int b = 36;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        double div = (a / b);
+        System.out.println(a + " " + "+" + " " + b + " = " + sum);
+        System.out.println(a + " " + "-" + b + " " + " = " + sub);
+        System.out.println(a + " " + "*" + b + " " + " = " + mul);
+        System.out.println(a + " " + "/" + b + " " + " = " + div);
+
+    }
+}
---
+++ TwoNumSum.java
@@ -0,0 +1,9 @@
+public class TwoNumSum {
+
+    public static void main(String[] args) {
+        int a = 38;
+        int b = 46;
+        int result = a + b;
+        System.out.print("The sum of "+ a + " + " +b + " is " + result);
+    }
+}
