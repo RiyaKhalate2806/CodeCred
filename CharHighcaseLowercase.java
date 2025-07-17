74360afcfe007619c94deae260d9487ffff9bf0f
RiyaKhalate2806 <riyakhalate289@gmail.com>
2025-07-17 23:32:33 +0530

    Add files via upload

---
+++ CharHighcaseLowercase.java
@@ -0,0 +1,11 @@
+public class CharHighcaseLowercase {
+    public static void main(String[] args) {
+        char ch = 'Y';
+        if ((ch >= 'A' && ch <= 'Z')) {
+            System.out.println("This is a Higher case");
+        }
+        else if(ch >= 'a' && ch <= 'z'){
+            System.out.println("This is a Lower case");
+        } 
+    }
+}
